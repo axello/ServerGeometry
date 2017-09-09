6d1a983da187a61783c67b0a3ed2dedb03199af1
Axel Roest <axel@phlux.us>
2017-09-09 15:43:41 +0200

    Added Framework target

---
+++ ServerGraphics/ServerGraphics.h
@@ -0,0 +1,19 @@
+//
+//  ServerGraphics.h
+//  ServerGraphics
+//
+//  Created by Axel Roest on 09/09/2017.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ServerGraphics.
+FOUNDATION_EXPORT double ServerGraphicsVersionNumber;
+
+//! Project version string for ServerGraphics.
+FOUNDATION_EXPORT const unsigned char ServerGraphicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ServerGraphics/PublicHeader.h>
+
+
